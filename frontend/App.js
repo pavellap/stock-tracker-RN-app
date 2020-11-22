37f44c4a14b9c250146519cdd4fff447e886d642
pavellap <trigognight@gmail.com>
2020-11-22 11:01:34 +0300

    ADDED: app.py file for server definition

--- frontend/App.js
+++ frontend/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color: "#ccc"}}>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
