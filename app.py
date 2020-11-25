9b0bc8edd901f7ff18c3c6c94e6af44d3fdf75a6
ThreeEightSeven <Arxange1o@yandex.ru>
2020-11-25 16:32:38 +0300

    Set up Flask

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 #  todo: basic flask app api setup (библиотеки подключи бля ежжи, братОК!)
 #  UPD: сука, пока писал это, ко мне в пиво муха залетела, суккккааааа
 from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Kick Pashu'
+
+
+if __name__ == "__main__":
+    app.run()
