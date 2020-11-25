ff1184415e55ebd0b23758576c38fdc75474cfeb
ThreeEightSeven <Arxange1o@yandex.ru>
2020-11-25 18:49:57 +0300

    set up REST api

---
+++ quote.py
@@ -0,0 +1,8 @@
+class Quote(Resource):
+    def get(self, id=0):
+        if id == 0:
+            return random.choice(ai_quotes), 200
+        for quote in ai_quotes:
+            if(quote["id"] == id):
+                return quote, 200
+        return "Quote not found", 404
