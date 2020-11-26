6a626b030ec0b80a0579b37e717d36a096db8daf
ThreeEightSeven <Arxange1o@yandex.ru>
2020-11-26 20:16:15 +0300

    API + Mongo

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, render_template, request
 from flask_restful import Api, Resource, reqparse
 import random
-import db
+from db import Table
 
 app = Flask(__name__)
 api = Api(app)
@@ -101,53 +101,41 @@ ai_quotes = [
 
 
 class Quote(Resource):
-    def get(self, id=0):
-        if id == 0:
-            return random.choice(ai_quotes), 200
-        for quote in ai_quotes:
-            if quote["id"] == id:
-                return quote, 200
-        return "Quote not found", 404
+    def get(self, id):
+        try:
+            return Table.find_one({'ID': id}), 200
+        except:
+            return "Line not found", 404
 
     def post(self, id):
         parser = reqparse.RequestParser()
-        parser.add_argument("author")
-        parser.add_argument("quote")
-        params = parser.parse_args()
-        for quote in ai_quotes:
-            if id == quote["id"]:
-                return f"Quote with id {id} already exists", 400
-        quote = {
-            "id": int(id),
-            "author": params["author"],
-            "quote": params["quote"]
+        parser.add_argument("Name")
+        values = parser.parse_args()
+        line = {
+            "Name": values["Name"]
         }
-        ai_quotes.append(quote)
-        return quote, 201
+        try:
+            Table.update({'ID': id}, {set: line})
+            return line, 201
+        except:
+            return f"Line with id {id} already exists", 400
 
     def put(self, id):
         parser = reqparse.RequestParser()
-        parser.add_argument("author")
-        parser.add_argument("quote")
-        params = parser.parse_args()
-        for quote in ai_quotes:
-            if id == quote["id"]:
-                quote["author"] = params["author"]
-                quote["quote"] = params["quote"]
-                return quote, 200
-
-        quote = {
-            "id": id,
-            "author": params["author"],
-            "quote": params["quote"]
-        }
-
-        ai_quotes.append(quote)
-        return quote, 201
+        parser.add_argument("Name")
+        values = parser.parse_args()
+        if Table.find_one({'ID': id}):
+            return f"Line with id {id} already exists", 400
+        else:
+            line = {
+                "ID": id,
+                "Name": values["Name"]
+            }
+            Table.insert_one(line)
+            return line, 201
 
     def delete(self, id):
-        global ai_quotes
-        ai_quotes = [qoute for qoute in ai_quotes if qoute["id"] != id]
+        Table.delete_one({'ID': id})
         return f"Quote with id {id} is deleted.", 200
 
 
